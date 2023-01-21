c23244b25eef6466835a2c4183f8c130ef66b349
Petrus van Egeraat <pjj.vanegeraat@gmail.com>
2023-01-21 11:48:52 +0400

    Feat: tailwind + download contact button

---
+++ web/src/components/Contact.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const VCards = (props: Props) => {
+  return (
+    <div className="h-screen flex grow items-center justify-center">
+      <button className="h-10 bg-red-500">Download contact</button>
+    </div>
+  );
+};
+
+export default VCards;
