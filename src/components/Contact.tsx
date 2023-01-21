d82f6e1d403d46ae3f32e91bfe1fc8ab1de37b42
Petrus van Egeraat <pjj.vanegeraat@gmail.com>
2023-01-21 13:10:37 +0400

    Feat: call lambda from FE

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,20 @@
+import { useCallback } from "react";
+import { API } from "aws-amplify";
+
 type Props = {};
 
 const VCards = (props: Props) => {
+  const downloadContact = useCallback(async () => {
+    const response = await API.get("qrsapi", "/downloadcontact", {});
+    console.log(1337, "data", response);
+    alert(response);
+  }, []);
+
   return (
     <div className="h-screen flex grow items-center justify-center">
-      <button className="h-10 bg-red-500">Download contact</button>
+      <button onClick={downloadContact} className="h-10 bg-red-500">
+        Download contact
+      </button>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// setup amplify
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
